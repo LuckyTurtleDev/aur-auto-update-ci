7a15a2d7bfabaa0b4f4571f3718a8fb031d04ab6
LuckyTurtleDev <git@lukas1818.de>
2023-02-19 18:46:45 +0100

    makpkg

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
-use anyhow::{self, Context};
+use anyhow::{bail, Context};
 use basic_toml;
 use clap::Parser;
 use serde::{Deserialize, Serialize};
 use serde_json;
 use srcinfo::Srcinfo;
-use std::{fs, io, path::PathBuf};
+use std::{
+	ffi::OsStr,
+	fs, io,
+	io::Read,
+	path::{Path, PathBuf},
+};
 
 mod github;
 
@@ -13,7 +18,7 @@ struct Index {
 	#[serde(default)]
 	i: u8,
 	#[serde(default)]
-	version: String,
+	tag: String,
 }
 
 trait Tags {
@@ -62,13 +67,39 @@ fn main() {
 			eprintln!("{:?}", err.context(format!("ERROR processing package {package}")));
 		}
 		println!();
+		println!();
 	}
+}
+
+fn run<P, S>(program: &str, args: Option<&[S]>, dir: P, show_output: bool) -> anyhow::Result<Vec<u8>>
+where
+	P: AsRef<Path>,
+	S: AsRef<OsStr>,
+{
+	use std::process::Command;
+	let args: &[S] = args.unwrap_or_default();
+	let mut command = Command::new(program);
+	let command = command.current_dir(dir).args(args);
+	let output = if show_output {
+		let child = command.spawn().with_context(|| format!("failed to start {program:?}"))?;
+		child.wait_with_output()?
+	} else {
+		let output = command.output().with_context(|| format!("failed to start {program:?}"))?;
+		let stderr = String::from_utf8_lossy(&output.stderr);
+		print!("{}", stderr);
+		output
+	};
+	if !output.status.success() {
+		bail!("{program:?} has exit with exit code {}", output.status);
+	}
+	Ok(output.stdout)
 }
 
 fn progess_package(package: &str, opt: &Opt) -> anyhow::Result<()> {
 	println!("==> process package: {}", package);
+	let dir = package;
 	println!("-> load .index.json");
-	let file_content = match fs::read_to_string(PathBuf::from(package).join(".index.json")) {
+	let file_content = match fs::read_to_string(PathBuf::from(dir).join(".index.json")) {
 		Ok(value) => value,
 		Err(err) => {
 			if err.kind() != io::ErrorKind::NotFound {
@@ -78,13 +109,15 @@ fn progess_package(package: &str, opt: &Opt) -> anyhow::Result<()> {
 		},
 	};
 	let index: Index = serde_json::from_str(&file_content).with_context(|| "failed to prase .index.json")?;
+
 	println!("-> load config.toml");
 	let config: Config = basic_toml::from_str(
-		&fs::read_to_string(PathBuf::from(package).join("ci.toml")).with_context(|| "failed to open `ci.toml`")?,
+		&fs::read_to_string(PathBuf::from(dir).join("ci.toml")).with_context(|| "failed to open `ci.toml`")?,
 	)
 	.with_context(|| "failed to prase `ci.toml`")?;
+
 	println!("-> load .SRCINFO");
-	let old_pkgver = match fs::read(PathBuf::from(package).join(".SRCINFO")) {
+	let old_pkgver = match fs::read(PathBuf::from(dir).join(".SRCINFO")) {
 		Ok(value) => Some(
 			Srcinfo::parse_buf(&*value)
 				.with_context(|| "failed to prase .SRCINFO")?
@@ -99,7 +132,43 @@ fn progess_package(package: &str, opt: &Opt) -> anyhow::Result<()> {
 		},
 	};
 	let tags = config.source.get_tags().with_context(|| "failed to get tags")?;
+
 	println!("tags: {tags:?}");
 	let tag = tags.first().expect("no suitable tag found");
+	if &index.tag == tag {
+		println!("package is already uptodate");
+		return Ok(());
+	}
+
+	println!("-> modify PKGBUILD");
+	let mut pkgbuild = "".to_owned();
+	for line in fs::read_to_string(PathBuf::from(dir).join("PKGBUILD"))
+		.with_context(|| "failed to open `PKGBUILD`")?
+		.split('\n')
+	{
+		if line.starts_with("_pkgtag=") {
+			pkgbuild += &format!("_pkgtag={tag}");
+		} else {
+			pkgbuild += line;
+		}
+		pkgbuild += "\n";
+	}
+	pkgbuild.pop(); //avoid adding an additonal newline at file end
+	fs::write(PathBuf::from(dir).join("PKGBUILD"), pkgbuild).with_context(|| "failed to write PKGGBUILD")?;
+
+	println!("-> updpkgsums");
+	run::<_, &str>("updpkgsums", None, dir, true)?;
+
+	println!("-> makepkg --printsrcinfo");
+	let stdout = run("makepkg", Some(&["--printsrcinfo"]), dir, false)?;
+	let pkgver = Srcinfo::parse_buf(&*stdout)
+		.with_context(|| "failed to prase .SRCINFO")?
+		.base
+		.pkgver;
+	//TODO: check if pkgver is valid
+	fs::write(PathBuf::from(dir).join(".SRCINFO"), stdout).with_context(|| "failed to write .SRCINFO")?;
+
+	println!("-> makepkg");
+	run("makepkg", Some(&["--syncdeps", "--check", "--noarchive"]), dir, true)?;
 	Ok(())
 }
